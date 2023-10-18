09a76a610d54f6a82067afa4de6fe7768cb47c84
abhinavtariyal <abhinavtariyal@gmail.com>
2023-10-18 13:03:58 +1100

    minor changes to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,17 @@
 import { Button } from "@material-tailwind/react";
 import { useRouter } from "next/router";
 import { UnderlineTabs } from "./NavBar";
+import Link from "next/link";
 
 const Header = () => {
   const router = useRouter();
   return (
-    <header className="bg-[#FFF] sticky top-0 z-10 shadow">
+    <header className="bg-[#FFF] sticky top-0 z-10 shadow-xl">
       <nav className="flex justify-between items-center w-[94%] mx-auto">
         <div className="w-[300px] h-[84px]">
-          <img src="/Frame 8.png" className="object-cover h-full" />
+          <Link href="/">
+            <img src="/Frame 8.png" className="object-cover h-full" />
+          </Link>
         </div>
         <div className="w-[30%]">
           <UnderlineTabs />
