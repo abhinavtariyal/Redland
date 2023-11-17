59440dc0a954913d7af7823bea597848b1ba31f5
abhinavtariyal <abhinavtariyal@gmail.com>
2023-11-17 12:56:59 +1100

    changes to meta tag

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Redland Immigration Consultancy</title>
+        <title>Home</title>
         <meta
           name="description"
           content="Your trusted immigration consultancy agency for seamless visa and migration services. We provide expert guidance for your immigration needs."
@@ -13,6 +13,7 @@ const Home = () => {
           name="keywords"
           content="immigration consultancy, visa services, migration consultants, Redland Immigration, immigration experts, best immigration services, Victoria, Australia, Immigration, migration, Canada, Australia, UK, America work permit, job, permanent residency, refuge, refugee, skilled worker, lawyers, US, visa, visit visa, visitor, employment, migrate, skilled immigration, Portugal, Visa, Approval, best immigration services, best visa agent, best immigration agent, immigration agent, melbourne, best immigration agent melbourne"
         ></meta>
+        <meta property="og:title" content="Redland Immigration Consultancy"></meta>
       </Head>
       <HomePage />
     </>
