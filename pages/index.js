5476a042e533652b1c04b494cf3143d6246c0ef3
abhinavtariyal <abhinavtariyal@gmail.com>
2023-11-17 12:47:33 +1100

    improved seo

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Redland Immigration Consultancy</title>
         <meta
           name="description"
           content="Your trusted immigration consultancy agency for seamless visa and migration services. We provide expert guidance for your immigration needs."
         />
         <meta
           name="keywords"
-          content="immigration consultancy, visa services, migration consultants, Redland Immigration, immigration experts, best immigration services, Victoria, Australia"
+          content="immigration consultancy, visa services, migration consultants, Redland Immigration, immigration experts, best immigration services, Victoria, Australia, Immigration, migration, Canada, Australia, UK, America work permit, job, permanent residency, refuge, refugee, skilled worker, lawyers, US, visa, visit visa, visitor, employment, migrate, skilled immigration, Portugal, Visa, Approval, best immigration services, best visa agent, best immigration agent, immigration agent, melbourne, best immigration agent melbourne"
         ></meta>
       </Head>
       <HomePage />
